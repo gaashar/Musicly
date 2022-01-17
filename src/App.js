d9332fdfd2d82534cec662ce79b4e4df92a2e206
Aasha Govindaraj <gaasha@athenahealth.com>
2022-01-17 17:26:13 +0530

    Added the Studio Map section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Header } from "./components/header/header";
 import { ProductDetails } from "./components/productDetails/productDetails";
+import { StudioMap } from "./components/studioMap/studioMap";
 
 function App() {
   return (
     <div>
       <Header />
       <ProductDetails />
+      <StudioMap />
     </div>
   );
 }
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./button.scss";
 
-export const Button = ({ text, searchClass, seeMoreClass }) => {
+export const Button = ({
+  text,
+  searchClass,
+  seeMoreClass,
+  getDirectionClass,
+}) => {
   let addClass = "button";
   if (searchClass) addClass = addClass.concat(" search");
   if (seeMoreClass) addClass = addClass.concat(" seeMore");
+  if (getDirectionClass) addClass = addClass.concat(" getDirection");
   return <button className={addClass}>{text}</button>;
 };
